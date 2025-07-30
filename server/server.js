42bd7d685620738c9685caaad734fbc9b79aa20e
TungDucVu <vnvuductung2005@gmail.com>
2025-07-30 13:40:24 +0530

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ await connectDB()
 await connectCloudinary()
 
 // alow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://mern-grocery-app-p4g7.vercel.app']
 
 // midleware
 app.use(express.json())
